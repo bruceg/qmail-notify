a21cb006a6d84694b663fe4984d331f7034296a4
Bruce Guenter <bruce@untroubled.org>
2014-02-21 15:54:31 -0600

    qmail-notify-make: Eliminate use of stdio

--- qmail-notify-make.c
+++ qmail-notify-make.c
@@ -1,37 +1,37 @@
 #include <fcntl.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 #include <systime.h>
+#include <iobuf/obuf.h>
 #include <misc/misc.h>
 #include <msg/msg.h>
 
 #include "qmail-notify.h"
 
-static const char* bounce_header =
-"From: <MAILER-DAEMON@%s>\n"
-"To: <%s>\n"
-"Subject: delayed delivery notice\n";
+static const char* bounce_header = "{"
+"From: <MAILER-DAEMON@}s{>\n"
+"To: <}s{>\n"
+"Subject: delayed delivery notice\n}";
 
-static const char* mime_bounce_header =
+static const char* mime_bounce_header = "{"
 "MIME-Version: 1.0\n"
-"Content-Type: multipart/mixed; boundary=\"%s\"\n"
+"Content-Type: multipart/mixed; boundary=\"}s{\"\n"
 "\n"
 "This is a multi-part message in MIME format.\n"
 "(If you can see this message, your E-mail client is not MIME compatible.)\n"
-"--%s\n"
+"--}s{\n"
 "Content-Type: text/plain; charset=us-ascii\n"
-"Content-Transfer-Encoding: 7bit\n";
+"Content-Transfer-Encoding: 7bit\n}";
 
-static const char* bounce_body =
-"Your message has been received by %s but has been\n"
-"undeliverable to the following recipients for at least %s.\n"
+static const char* bounce_body = "{"
+"Your message has been received by }s{ but has been\n"
+"undeliverable to the following recipients for at least }s{.\n"
 "The mail system will continue to attempt to deliver your message\n"
-"to these recipients for a total of %s.  You do not need to\n"
-"resend your message at this time.\n";
+"to these recipients for a total of }s{.  You do not need to\n"
+"resend your message at this time.\n}";
 
 static const char* recipients_prefix = "\nRecipient(s):\n";
 
@@ -40,16 +40,16 @@ static const char* message_seperator =
 "--- Below this line is a copy of the original message.\n"
 "\n";
 
-static const char* mime_message_seperator =
+static const char* mime_message_seperator = "{"
 "\n"
 "The following attachment contains a copy of the original message.\n"
 "\n"
-"--%s\n"
+"--}s{\n"
 "Content-Type: message/rfc822\n"
 "Content-Disposition: inline\n"
-"\n";
+"\n}";
 
-static const char* mime_message_end = "\n--%s--";
+static const char* mime_message_end = "{\n--}s{--}";
 
 static char mime_boundary[65];
 
@@ -65,22 +65,22 @@ static void make_mime_boundary(void)
   mime_boundary[i] = 0;
 }
 
-static void copy_message(FILE* out, const char* filename)
+static void copy_message(obuf* out, const char* filename)
 {
   int fd = open_file("mess", filename);
   char buf[4096];
   if(fd == -1)
     die1sys(111, "Could not open message file");
   if(opt_mime)
-    fprintf(out, mime_message_seperator, mime_boundary);
+    obuf_putf(out, mime_message_seperator, mime_boundary);
   else
-    fputs(message_seperator, out);
+    obuf_puts(out, message_seperator);
   if(opt_msgbytes < 0)
     for(;;) {
       ssize_t rd = read(fd, buf, sizeof buf);
       if(rd <= 0)
 	break;
-      if(fwrite(buf, rd, 1, out) != 1)
+      if (!obuf_write(out, buf, rd))
 	break;
     }
   else {
@@ -91,17 +91,17 @@ static void copy_message(FILE* out, const char* filename)
       ssize_t rd = read(fd, buf, tord);
       if(rd <= 0)
 	break;
-      if(fwrite(buf, rd, 1, out) != 1)
+      if (!obuf_write(out, buf, rd))
 	break;
       total += rd;
     }
     if(total >= opt_msgbytes)
-      fputs("[...]", out);
+      obuf_puts(out, "[...]");
   }
   close(fd);
   if(opt_mime)
-    fprintf(out, mime_message_end, mime_boundary);
-  fputc('\n', out);
+    obuf_putf(out, mime_message_end, mime_boundary);
+  obuf_putc(out, '\n');
 }
 
 void load_bounce_body(const char* filename)
@@ -142,27 +142,28 @@ void make_bounce_body(int fd, const char* sender, const char* filename,
   const char* ptr;
   char time1[128];
   char time2[128];
-  FILE* out;
+  obuf out;
 
-  out = fdopen(fd, "w");
+  obuf_init(&out, fd, 0, 0, 0);
   time2str(age, time1);
   time2str(queuelifetime, time2);
-  fprintf(out, bounce_header, me, sender);
+
+  obuf_putf(&out, bounce_header, me, sender);
   if(opt_mime) {
     make_mime_boundary();
-    fprintf(out, mime_bounce_header, mime_boundary, mime_boundary);
+    obuf_putf(&out, mime_bounce_header, mime_boundary, mime_boundary);
   }
-  fputs("\n", out);
-  fprintf(out, bounce_body, me, time1, time2);
-  fputs(recipients_prefix, out);
+  obuf_puts(&out, "\n");
+  obuf_putf(&out, bounce_body, me, time1, time2);
+  obuf_puts(&out, recipients_prefix);
   for(ptr = locals; *ptr; ptr += strlen(ptr)+1)
     if(*ptr == 'T')
-      fprintf(out, "\t%s\n", ptr+1);
+      obuf_putf(&out, "{\t}s{\n}", ptr+1);
   for(ptr = remotes; *ptr; ptr += strlen(ptr)+1)
     if(*ptr == 'T')
-      fprintf(out, "\t%s\n", ptr+1);
+      obuf_putf(&out, "{\t}s{\n}", ptr+1);
   if(opt_msgbytes)
-    copy_message(out, filename);
-  if(fflush(out) != 0)
+    copy_message(&out, filename);
+  if (!obuf_flush(&out))
     die1sys(111, "Could not write bounce message");
 }
